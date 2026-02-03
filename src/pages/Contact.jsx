604867d00a346eb6fa85668fda1ea971e3355e8b
dheerajrathore007 <rathoredheeraj390@gmail.com>
2026-02-03 12:50:53 +0530

    Update UI and fixes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,71 @@
+import { useState } from "react";
+
 export default function Contact() {
-return (
-<section className="max-w-3xl mx-auto py-20 px-4">
-<h2 className="text-3xl font-bold mb-6">Contact Me</h2>
-<form className="space-y-4">
-<input type="text" placeholder="Your Name" className="w-full p-3 border rounded" />
-<input type="email" placeholder="Your Email" className="w-full p-3 border rounded" />
-<textarea placeholder="Your Message" className="w-full p-3 border rounded"></textarea>
-<button className="px-6 py-2 bg-blue-600 text-white rounded">Send</button>
-</form>
-</section>
-);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const whatsappNumber = "916261672551";
+
+        const text = `
+Hi Dheeraj, I want to connect with you 👋
+MY name is ${name} ,
+my E-mail id is ${email}
+Message:
+${message}
+    `;
+
+        const encodedText = encodeURIComponent(text);
+
+        const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodedText}`;
+
+        window.open(whatsappURL, "_blank");
+    };
+
+    return (
+        <section className="max-w-4xl mx-auto py-20 px-4">
+            <h2 className="text-3xl font-bold mb-8">Contact Me</h2>
+
+            <form onSubmit={handleSubmit} className="space-y-6">
+
+                <input
+                    type="text"
+                    placeholder="Your Name"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="w-full p-3 border rounded"
+                />
+
+                <input
+                    type="email"
+                    placeholder="Your Email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full p-3 border rounded"
+                />
+
+                <textarea
+                    placeholder="Your Message"
+                    rows="2"
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="w-full p-3 border rounded"
+                />
+
+                <button
+                    type="submit"
+                    className="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition"
+                >
+                    send
+                </button>
+
+            </form>
+        </section>
+    );
 }
